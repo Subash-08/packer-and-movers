40663eb2109f5c31cb7f0a892912655f09c0615c
Subash-08 <128483625+Subash-08@users.noreply.github.com>
2024-10-06 22:15:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const showmenu = document.getElementById("menu")
+const showul = document.querySelector("nav ul")
+
+showmenu.addEventListener("click" ,() => {
+    showul.classList.toggle("showmenu")
+
+})
+
+showul.addEventListener("click",() => {
+    showul.classList.toggle("showmenu")
+})
