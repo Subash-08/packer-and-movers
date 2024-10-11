cb9842c3631aca06f3617bad64d9eaed9d31871f
Subash-08 <128483625+Subash-08@users.noreply.github.com>
2024-10-11 12:02:14 +0530

    css updated

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ const showul = document.querySelector("nav ul")
 
 showmenu.addEventListener("click" ,() => {
     showul.classList.toggle("showmenu")
-    console.log("njkbi");
-    
-
-})
+   })
 
 showul.addEventListener("click",() => {
     showul.classList.toggle("showmenu")
